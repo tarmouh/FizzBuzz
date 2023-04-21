3e3ffad1abc8ccc81445740bd0a10bcf15cb7a41
mmouh <mmouh@172.17.138.93>
2023-04-21 14:12:34 +0200

    Commit initial

---
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
